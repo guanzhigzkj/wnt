81be73b66a7e6e5c60fbec4f812dce083463f186
songanqi@baojia.com <songanqi@baojia.com>
2019-01-29 18:01:55 +0800

    bjj

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,3 +5,13 @@
  * Date: 2019/1/29
  * Time: 17:53
  */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login() {
+        return view('user.login');
+    }
+}
